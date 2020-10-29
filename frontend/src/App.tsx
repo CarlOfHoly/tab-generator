50bd8c855918d868b67334f31129709f1149d985
carlofholy <carl.smestad@gmail.com>
2020-10-29 21:08:08 +0100

    setup routing and tuner page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
 import React from "react";
-import Frontpage from "./components/Frontpage";
+import Frontpage from "./components/TabPage";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import TunerPage from "./components/TunerPage";
 
 function App() {
   return (
     <div className="App">
-      <Frontpage />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/tabs">
+            <Frontpage />
+          </Route>
+          <Route path="/tuner">
+            <TunerPage />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
