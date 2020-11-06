a6b0497c1123c9fbd06372976163a718560ef4d4
carlofholy <carl.smestad@gmail.com>
2020-11-06 20:16:21 +0100

    add User schema and database config

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -5,6 +5,10 @@ from werkzeug.utils import secure_filename
 from flask_cors import CORS
 import logging 
 
+import flask_sqlalchemy
+import flask_praetorian
+
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('INIT')
 
@@ -34,3 +38,34 @@ def uploadFile():
     file.save(destination)
     session['uploadFilePath']=destination
     return "Successfully uploaded file"
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Text, unique=True)
+    password = db.Column(db.Text)
+    roles = db.Column(db.Text)
+    is_active = db.Column(db.Boolean, default=True, server_default='true')
+
+    @classmethod
+    def identify(cls, id):
+        return cls.query.get(id)
+
+    @classmethod
+    def lookup(cls, username):
+        return cls.query.filter_by(username=username).one_or_none()
+
+    @property
+    def identity(self):
+        return self.id
+
+    def is_valid(self):
+        return self.is_active
+
+
+guard.init_app(app, User)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(os.getcwd(), 'database.db')}"
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
